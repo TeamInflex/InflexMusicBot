fc5b52781145da13b76cdc002b617ba6a6ab1be7
Team Inflex <TheTeamInflex@Gmail.com>
2023-10-29 16:15:34 +0530

    v2.0 Stable !
    v2.0 Stable !

--- InflexMusic/utils/inline/start.py
+++ InflexMusic/utils/inline/start.py
@@ -21,7 +21,7 @@ def private_panel(_):
         [
             InlineKeyboardButton(
                 text=_["S_B_3"],
-                url=f"https://t.me/{app.username}?startgroup=true",
+                url=f"https://t.me/{app.username}?startgroup=s&admin=delete_messages+manage_video_chats+pin_messages+invite_users",
             )
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
