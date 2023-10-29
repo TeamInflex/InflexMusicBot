d9d7d77b76b0cc51978f050f3d679989c6117933
Team Inflex <TheTeamInflex@Gmail.com>
2023-10-29 09:24:53 +0530

    Update start.py
    v2.0 Stable !

--- InflexMusic/utils/inline/start.py
+++ InflexMusic/utils/inline/start.py
@@ -31,6 +31,7 @@ def private_panel(_):
         ],
         [
             InlineKeyboardButton(text=_["S_B_5"], user_id=config.OWNER_ID),
+            InlineKeyboardButton(text=_["S_B_7"], url=config.UPSTREAM_REPO),
         ],
     ]
     return buttons
