524f1593533fcd502810d457d951069ea9e5d2d2
Team Inflex <TheTeamInflex@Gmail.com>
2023-10-29 09:36:29 +0530

    Update settings.py
    v2.0 Stable !

--- InflexMusic/utils/inline/settings.py
+++ InflexMusic/utils/inline/settings.py
@@ -25,7 +25,7 @@ def setting_markup(_):
 def vote_mode_markup(_, current, mode: Union[bool, str] = None):
     buttons = [
         [
-            InlineKeyboardButton(text="Vᴏᴛɪɴɢ ᴍᴏᴅᴇ ➜", callback_data="VOTEANSWER"),
+            InlineKeyboardButton(text="𝖵𝗈𝗍𝗂𝗇𝗀 𝖬𝗈𝖽𝖾 ➜", callback_data="VOTEANSWER"),
             InlineKeyboardButton(
                 text=_["ST_B_5"] if mode == True else _["ST_B_6"],
                 callback_data="VOMODECHANGE",
