84a70d901a3dc963ea3a5a861d2a0a7f1c623a0d
Team Inflex <TheTeamInflex@Gmail.com>
2023-12-20 09:38:36 +0530

    v2.0 Stable !
    v2.0 Stable !

--- InflexMusic/plugins/tools/reload.py
+++ InflexMusic/plugins/tools/reload.py
@@ -87,13 +87,15 @@ async def restartbot(client, message: Message, _):
 
 
 @app.on_callback_query(filters.regex("close") & ~BANNED_USERS)
-async def close_menu(_, CallbackQuery):
+async def close_menu(_, query: CallbackQuery):
     try:
-        await CallbackQuery.answer()
-        await CallbackQuery.message.delete()
-        await CallbackQuery.message.reply_text(
-            f"➜ 𝖢𝗅𝗈𝗌𝖾𝖽 𝖡𝗒 : {CallbackQuery.from_user.mention}"
+        await query.answer()
+        await query.message.delete()
+        umm = await query.message.reply_text(
+            f"➜ 𝖢𝗅𝗈𝗌𝖾𝖽 𝖡𝗒 : {query.from_user.mention}"
         )
+        await asyncio.sleep(7)
+        await umm.delete()
     except:
         pass
 
