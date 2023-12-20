370d2f10d4cff68ac944425fa8bc2c966e70685e
Team Inflex <TheTeamInflex@Gmail.com>
2023-12-20 09:48:53 +0530

    v2.0 Stable !
    v2.0 Stable !

--- InflexMusic/utils/decorators/language.py
+++ InflexMusic/utils/decorators/language.py
@@ -1,8 +1,8 @@
 from strings import get_string
 
-from AnonXMusic import app
-from AnonXMusic.misc import SUDOERS
-from AnonXMusic.utils.database import get_lang, is_maintenance
+from InflexMusic import app
+from InflexMusic.misc import SUDOERS
+from InflexMusic.utils.database import get_lang, is_maintenance
 
 
 def language(mystic):
